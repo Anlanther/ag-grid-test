a77b0c17192589670182d31b68995971b568690b
Anlanther <anlanther@gmail.com>
2024-02-24 18:55:05 +0800

    build: imported storybook

--- src/app/grid/pure-grid/pure-grid.component.ts
+++ src/app/grid/pure-grid/pure-grid.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-
+import { AgGridAngular } from 'ag-grid-angular'; // AG Grid Component
+import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
 @Component({
   selector: 'app-pure-grid',
   standalone: true,
-  imports: [],
+  imports: [AgGridAngular],
   templateUrl: './pure-grid.component.html',
-  styleUrl: './pure-grid.component.scss'
+  styleUrl: './pure-grid.component.scss',
 })
 export class PureGridComponent {
+  rowData = [
+    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+  ];
 
+  colDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'electric' },
+  ];
 }
